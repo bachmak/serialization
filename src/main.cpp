c11cfb90cf59a0e58342aa3dcc9b44c9ef85cacb
Dmitry Kochetov <kochetov6969@outlook.com>
2020-08-26 12:43:11 +0700

    Test classes in separated file

--- src/main.cpp
+++ src/main.cpp
@@ -1,71 +1,26 @@
 #include "serialization.h"
+#include "examples.h"
 
-#include <iostream>
 #include <fstream>
-#include <vector>
-#include <sstream>
-#include <string>
 
 using namespace std;
-
-struct SimpleStruct
-{
-    SimpleStruct(int a = 1, double b = 1.0, float c = 1.0, char d = 'b') 
-        : a(a), b(b), c(c), d(d) {}
-
-private:
-    template <typename Stream>
-    void serialize(Serializer<Stream> s)
-    {
-        s & a;
-        s & b;
-        s & c;
-        s & d;
-    }
-
-    int a = 1;
-    double b = 1.0;
-    float c = 1.0;
-    char d = 'b';
-
-    friend struct Access;
-};
-
-class SimpleClass
-{
-public:
-    SimpleClass(int a, double b, float c, char d, SimpleStruct e, std::vector<int> f)
-        : a(a), b(b), c(c), d(d), e(e), f(f) {}
-    SimpleClass() = default;
-
-private:
-    friend struct Access;
-
-    template <typename Stream>
-    void serialize(Serializer<Stream> s)
-    {
-        s & a;
-        s & b;
-        s & c;
-        s & d;
-        s & e;
-        s & f;
-    }
-
-    int a = 0;
-    double b = 0.0;
-    float c = 0.0;
-    char d = 'a';
-    SimpleStruct e;
-    vector<int> f = {0, 0, 0, 0};
-};
 
 int main(int argc, char const *argv[])
 {
     {
         ofstream output("data.bin", ios_base::binary);
         Archive<ofstream> oa(output);
-        SimpleClass simple_1(23, 2.5, 3.14, 'd', SimpleStruct(24, 2.6, 3.15, 'e'), vector<int>({1, 2, 3, 4}));
+
+        SimpleClass simple_1(
+            23,
+            vector<int>({1, 2, 3, 4}), 
+            3.14, "new string",
+            SimpleStruct(
+                24, 2.6, 3.15, 'e',
+                deque<int>({1, 2, 8}),
+                forward_list<double>({5.0, 3.15, 6.26})), 
+            list<string>({"three", "two", ""}));
+
         oa << simple_1;
     }
 
