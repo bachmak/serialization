8271838839fcbc72ad892983c46279457517ad8f
Dmitry Kochetov <kochetov6969@outlook.com>
2020-08-31 21:30:07 +0700

    New tests (serialize/Access/nested classes)

--- include/tests.h
+++ include/tests.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "serializer.h"
+#include "test_runner.h"
+
 #include <array>
 #include <vector>
 #include <string>
@@ -8,14 +11,160 @@
 #include <forward_list>
 #include <iostream>
 
-#include "serializer.h"
-
-struct SimpleStruct
+class NotSerializableWithFriendAccess
 {
-    SimpleStruct(int a = 1, double b = 1.0, float c = 1.0,
-                 char d = 'b', std::deque<int> e = {},
-                 std::forward_list<double> f = {})
+public:
+    NotSerializableWithFriendAccess(int a, int b) : a(a), b(b) {}
+    NotSerializableWithFriendAccess() = default;
+
+    bool operator==(const NotSerializableWithFriendAccess& x) const
+    {
+        return a == x.a && b == x.b;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os,
+        const NotSerializableWithFriendAccess& x)
+    {
+        return os << '{' << x.a << ", " << x.b << '}';
+    }
+
+private:
+    friend struct Access;
+    int a = 1;
+    int b = 2;
+};
+
+class NotSerializableWithoutFriendAccess
+{
+public:
+    NotSerializableWithoutFriendAccess(int a, int b) : a(a), b(b) {}
+    NotSerializableWithoutFriendAccess() = default;
+
+    bool operator==(const NotSerializableWithoutFriendAccess& x) const
+    {
+        return a == x.a && b == x.b;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os,
+        const NotSerializableWithoutFriendAccess& x)
+    {
+        return os << '{' << x.a << ", " << x.b << '}';
+    }
+
+private:
+    int a = 1;
+    int b = 2;
+};
+
+class SerializableWithoutFriendAccess
+{
+public:
+    SerializableWithoutFriendAccess(int a, int b) : a(a), b(b) {}
+    SerializableWithoutFriendAccess() = default;
+
+    bool operator==(const SerializableWithoutFriendAccess& x) const
+    {
+        return a == x.a && b == x.b;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os,
+        const SerializableWithoutFriendAccess& x)
+    {
+        return os << '{' << x.a << ", " << x.b << '}';
+    }
+
+private:
+    template <typename Stream>
+    void serialize(Serializer<Stream> s)
+    {
+        s & a;
+        s & b;
+    }
+
+    int a = 1;
+    int b = 2;
+};
+
+class SerializableWithFriendAccess
+{
+public:
+    SerializableWithFriendAccess(int a, int b) : a(a), b(b) {}
+    SerializableWithFriendAccess() = default;
+
+    bool operator==(const SerializableWithFriendAccess& x) const
+    {
+        return a == x.a && b == x.b;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os,
+        const SerializableWithFriendAccess& x)
+    {
+        return os << '{' << x.a << ", " << x.b << '}';
+    }
+
+private:
+    friend struct Access;
+
+    template <typename Stream>
+    void serialize(Serializer<Stream> s)
+    {
+        s & a;
+        s & b;
+    }
+
+    int a = 1;
+    int b = 2;
+};
+
+class PublicSerializableWithoutFriendAccess
+{
+public:
+    PublicSerializableWithoutFriendAccess(int a, int b) : a(a), b(b) {}
+    PublicSerializableWithoutFriendAccess() = default;
+
+    bool operator==(const PublicSerializableWithoutFriendAccess& x) const
+    {
+        return a == x.a && b == x.b;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os,
+        const PublicSerializableWithoutFriendAccess& x)
+    {
+        return os << '{' << x.a << ", " << x.b << '}';
+    }
+
+    template <typename Stream>
+    void serialize(Serializer<Stream> s)
+    {
+        s & a;
+        s & b;
+    }
+
+private:
+    int a = 1;
+    int b = 2;
+};
+
+struct StructWithBasicTypesAndContainers
+{
+    StructWithBasicTypesAndContainers(int a = 1, double b = 1.0,
+                                      float c = 1.0, char d = 'b',
+                                      std::deque<int> e = {},
+                                      std::forward_list<double> f = {})
         : a(a), b(b), c(c), d(d), e(e), f(f) {}
+
+    bool operator==(const StructWithBasicTypesAndContainers& x) const
+    {
+        return a == x.a && b == x.b && c == x.c && 
+               d == x.d && e == x.e && f == x.f;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os,
+                                    const StructWithBasicTypesAndContainers& x)
+    {
+        return os << '{' << x.a << ", " << x.b << ", " << x.c << ", "
+                         << x.d << ", " << x.e << ", " << x.f << '}';
+    }
 
 private:
     friend struct Access;
@@ -39,13 +188,28 @@ private:
     std::forward_list<double> f = { 0.0, 0.0 };
 };
 
-class SimpleClass
+class ClassWithNestedStruct
 {
 public:
-    SimpleClass(int a, std::vector<int> b, float c,
-                std::string d, SimpleStruct e, std::list<std::string> f)
+    ClassWithNestedStruct(int a, std::vector<int> b, float c, std::string d,
+                          StructWithBasicTypesAndContainers e,
+                          std::list<std::string> f)
         : a(a), b(b), c(c), d(d), e(e), f(f) {}
-    SimpleClass() = default;
+
+    ClassWithNestedStruct() = default;
+
+    bool operator==(const ClassWithNestedStruct& x) const
+    {
+        return a == x.a && b == x.b && c == x.c && 
+               d == x.d && e == x.e && f == x.f;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os,
+                                    const ClassWithNestedStruct& x)
+    {
+        return os << '{' << x.a << ", " << x.b << ", " << x.c << ", "
+                         << x.d << ", " << x.e << ", " << x.f << '}';
+    }
 
 private:
     friend struct Access;
@@ -65,7 +229,7 @@ private:
     std::vector<int> b = {0, 0};
     float c = 0.0;
     std::string d = "Lorem ipsum dolor sit amet, consectetur adipiscing elit";
-    SimpleStruct e;
+    StructWithBasicTypesAndContainers e;
     std::list<std::string> f = {"one", "two"};
 };
 
@@ -77,15 +241,17 @@ public:
 
     PodClass() = default;
 
-    static void test();
-
     bool operator==(const PodClass& pod) const
     {
         return (a == pod.a && b == pod.b && c == pod.c && d == pod.d);
     }
 
-    friend std::ostream& operator<< (std::ostream& os, const PodClass& pod);
-;
+    friend std::ostream& operator<< (std::ostream& os, const PodClass& pod)
+    {
+        return os << '{' << pod.a << ", " << pod.b << ", " 
+                  << pod.c << ", " << pod.d << '}';
+    }
+
 private:
     int a = 0;
     char b = 'a';
@@ -111,5 +277,11 @@ void TestPointers();
 void TestReferences();
 
 void TestSequenceContainers();
+
+void TestSerializeAccessCombinations();
+
+void TestPodClass();
+
+void TestClassWithNestedStruct();
 
 void TestAll();
--- src/tests.cpp
+++ src/tests.cpp
@@ -9,17 +9,13 @@ using namespace std;
 void TestAll()
 {
     TestRunner tr;
-    RUN_TEST(tr, PodClass::test);
+    RUN_TEST(tr, TestPodClass);
     RUN_TEST(tr, TestBasicTypes);
     RUN_TEST(tr, TestPointers);
     RUN_TEST(tr, TestReferences);
     RUN_TEST(tr, TestSequenceContainers);
-}
-
-std::ostream& operator<< (std::ostream& os, const PodClass& pod)
-{
-    return os << '{' << pod.a << ", " << pod.b << ", " 
-              << pod.c << ", " << pod.d << '}';
+    RUN_TEST(tr, TestSerializeAccessCombinations);
+    RUN_TEST(tr, TestClassWithNestedStruct);
 }
 
 template <typename T, typename Stream>
@@ -255,22 +251,103 @@ void TestSequenceContainers()
     }
 }
 
-void PodClass::test()
+void TestSerializeAccessCombinations()
 {
+    NotSerializableWithFriendAccess       a(10, 20);
+    NotSerializableWithoutFriendAccess    b(30, 40);
+    SerializableWithoutFriendAccess       c(50, 60);
+    SerializableWithFriendAccess          d(70, 80);
+    PublicSerializableWithoutFriendAccess e(90, 100);
+    size_t fail_counter = 0;
+
+    {
+        ofstream output("data.bin", ios_base::binary);
+        Archive<ofstream> oa(output);
+
+        SerializeAndCountFails(a, oa, fail_counter);
+        SerializeAndCountFails(b, oa, fail_counter);
+        SerializeAndCountFails(c, oa, fail_counter);
+        SerializeAndCountFails(d, oa, fail_counter);
+        SerializeAndCountFails(e, oa, fail_counter);
+    }
+    {
+        ifstream input("data.bin", ios_base::binary);
+        Archive<ifstream> ia(input);
+
+        NotSerializableWithFriendAccess       new_a;
+        NotSerializableWithoutFriendAccess    new_b;
+        SerializableWithoutFriendAccess       new_c;
+        SerializableWithFriendAccess          new_d;
+        PublicSerializableWithoutFriendAccess new_e;
+
+        SerializeAndCountFails(new_a, ia, fail_counter);
+        SerializeAndCountFails(new_b, ia, fail_counter);
+        SerializeAndCountFails(new_c, ia, fail_counter);
+        SerializeAndCountFails(new_d, ia, fail_counter);
+        SerializeAndCountFails(new_e, ia, fail_counter);
+
+        ASSERT_NOT_EQUAL(new_a, a);
+        ASSERT_NOT_EQUAL(new_b, b);
+        ASSERT_NOT_EQUAL(new_c, c);
+        ASSERT_EQUAL(new_d, d);
+        ASSERT_EQUAL(new_e, e);
+
+        ASSERT_EQUAL(fail_counter, 6);
+    }
+}
+
+void TestPodClass()
+{
+    size_t fail_counter = 0;
     {
         ofstream output("data.bin", ios_base::binary);
         Archive<ofstream> oa(output);
 
         PodClass podClassInstance(-1, 'b', 2, 500);
-        oa << podClassInstance;
+        SerializeAndCountFails(podClassInstance, oa, fail_counter);
     }
     {
         ifstream input("data.bin", ios_base::binary);
         Archive<ifstream> ia(input);
 
         PodClass podClassInstance;
-        ia >> podClassInstance;
+        SerializeAndCountFails(podClassInstance, ia, fail_counter);
 
         ASSERT_EQUAL(podClassInstance, PodClass(-1, 'b', 2, 500));
+        ASSERT_FALSE(fail_counter);
+    }
+}
+
+void TestClassWithNestedStruct()
+{
+    ClassWithNestedStruct a(1,
+                            { 20, 30, 40 },
+                            500.00,
+                            "6k",
+                            StructWithBasicTypesAndContainers(2,
+                                                              0.35,
+                                                              3.1,
+                                                              'u',
+                                                              { 90, 100, 30 },
+                                                              { 0.5, 0.6 }),
+                            { "70k", "80k", "90k" });
+    size_t fail_counter = 0;
+
+    {
+        ofstream output("data.bin", ios_base::binary);
+        Archive<ofstream> oa(output);
+
+        SerializeAndCountFails(a, oa, fail_counter);
+    }
+    {
+        ifstream input("data.bin", ios_base::binary);
+        Archive<ifstream> ia(input);
+
+        ClassWithNestedStruct new_a;
+
+        SerializeAndCountFails(new_a, ia, fail_counter);
+
+        ASSERT_EQUAL(new_a, a);
+        ASSERT_FALSE(fail_counter);
     }
 }
